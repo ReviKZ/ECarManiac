04387d9924092220521fbed74448c53e2653c6a9
ReviKZ <kovacs.zoard.1@gmail.com>
2023-03-29 08:06:11 +0200

    Feat: Add static Manager class for DB connection testing

---
+++ ECarManiac/ECarManiac/Managers/CarDbManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+
+namespace ECarManiac.Managers
+{
+    public static class CarDbManager
+    {
+        public static bool TestConnetion(string ConnectionString)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
