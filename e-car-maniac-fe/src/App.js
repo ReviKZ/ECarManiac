7ec5232de71049760bc47ce9b30bc113419773b6
ReviKZ <kovacs.zoard.1@gmail.com>
2023-04-14 14:30:53 +0200

    Feat: Add Routing and Routes

--- e-car-maniac-fe/src/App.js
+++ e-car-maniac-fe/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          {/* Main Router Tag */}
+          <Routes>
+              {/* Layout Tag */}
+              <Route path="/" element="" >
+                  {/* Page Tags */}
+
+                  {/* Index Page / Navigation */}
+                  <Route path="" element="" />
+
+                  {/* Listing Page */}
+                  <Route path="get/list-all" element="" />
+
+                  {/* Id Specific Page */}
+                  <Route path="get/:id" element="" />
+
+                  {/* Adding Page */}
+                  <Route path="add" element="" />
+
+                  {/* Not-Found Page */}
+                  <Route path="*" element="" />
+              </Route>
+          </Routes>
+      </>
   );
 }
 
