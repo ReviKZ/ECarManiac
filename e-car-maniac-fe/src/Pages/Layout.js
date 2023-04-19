efa52f454a63d72a22a9230d92d70dca5033ed46
ReviKZ <kovacs.zoard.1@gmail.com>
2023-04-19 13:05:06 +0200

    Feat: Add NavBar placement to Layout

--- e-car-maniac-fe/src/Pages/Layout.js
+++ e-car-maniac-fe/src/Pages/Layout.js
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
     return (
         <main className="App">
+            <nav>Navbar</nav>
             <Outlet />
         </main>
     );
