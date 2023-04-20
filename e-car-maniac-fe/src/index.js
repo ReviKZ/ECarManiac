ac5ed9bacdd90e3a0fab3cb31550218ece289345
ReviKZ <kovacs.zoard.1@gmail.com>
2023-04-20 11:09:35 +0200

    Feat: Add bootstrap javascript

--- e-car-maniac-fe/src/index.js
+++ e-car-maniac-fe/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
